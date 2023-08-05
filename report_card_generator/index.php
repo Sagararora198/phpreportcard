ee8cadb17dfec8681da3e489ed986020862e114e
Sagararora198 <99186411+Sagararora198@users.noreply.github.com>
2023-08-06 00:13:39 +0530

    Update index.php

--- report_card_generator/index.php
+++ report_card_generator/index.php
@@ -5,7 +5,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $servername = "localhost";
     $username = "root";
-    $password = "Nhidalna@123";
+    $password = "";
     $database = "roles";
     $conn = mysqli_connect($servername,$username,$password,$database);
 
